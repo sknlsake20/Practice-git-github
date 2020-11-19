f6d62a7be128d531f421533cf8848704573f1646
shiva <shiva_arutla@mazenetsolution.com>
2020-11-18 22:42:24 -0600

    first commit from eclipse

---
+++ Practice-git-github/src/com/src/Calc.java
@@ -0,0 +1,17 @@
+package com.src;
+
+public class Calc {
+	
+	public int addition(int number1,int number2)
+	{
+		return number1+number2;
+	}
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Calc c=new Calc();
+		
+		c.addition(23, 45);
+	}
+
+}
